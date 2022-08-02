b32a1accc6db87d9604bee85d246d7d575c79a96
Tomislav <tkalec.tomi@gmail.com>
2022-08-02 13:11:44 +0200

    add: EasyAdmin

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -26,11 +26,16 @@ class Comment
     private $createdAt;
 
     #[ORM\ManyToOne(targetEntity: Conference::class, inversedBy: 'comments')]
-    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\JoinColumn(nullable: true)]
     private $conference;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $photoFilename;
+
+    public function __toString(): string
+    {
+        return (string) $this->getEmail();
+    }
 
     public function getId(): ?int
     {
